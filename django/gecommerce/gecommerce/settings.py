24825a719d8dacbfe04bef262a144bd5010ac209
Alan Suzuki <alandealmeidasuzuki@gmail.com>
2019-03-31 19:10:36 -0300

    versao base do html

--- django/gecommerce/gecommerce/settings.py
+++ django/gecommerce/gecommerce/settings.py
@@ -129,4 +129,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR
+STATICFILES_DIRS = (
+    os.path.join(STATIC_ROOT, 'static/'),
+)
 STATIC_URL = '/static/'
